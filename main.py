9062adff854509b823b78846f32c9fa05514a4a0
Mary Karroqe <mkarroqe@users.noreply.github.com>
2015-07-25 19:02:28 -0400

    Update main.py
    You can now decrypt all possibilities automatically, without manually entering key guesses one at a time.

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def init():
 		decrypt()
 	else:
 		print("I said xor, not or... rude.")
-		quit()
+		exit()
 
 def main():
-	choice = raw_input("Would you like to test another key? Enter 'yes' or 'no'.")
+	choice = raw_input("Would you like to test another key? Enter 'yes' or 'no'. ")
 	
 	if choice == "yes":
 		decrypt()
@@ -19,11 +19,22 @@ def main():
 		print("k.")
 		quit()
 	else:
-		print("Error :/")
+		print("Error :/ ")
 		quit()
 
 	init()
-
+'''
+def main2():
+	choice = raw_input("Would you like to decrypt your message? Enter 'yes' or 'no'. ")
+    if choice == "yes":
+        print("Aha.. I see you don't trust me.  That's okay, here's the original message: ")
+        decrypt()
+    elif choice == "no":
+        print("k.")
+        quit()
+    else:
+        print("Error. Way to break me :/")
+'''
 def encrypt():
 	plaintext = raw_input("Please enter the message you want to encrypt: ")
 	key = raw_input("Please enter your key: ")
@@ -36,34 +47,40 @@ def encrypt():
 		ciphertext += chr(y)
 	
 	print(ciphertext)
-	main()
 
 def decrypt():
 	plaintext = raw_input("Please enter the message you want to decrypt: ")
-	key = raw_input("Please enter your key: ")
+	ifkey = raw_input("Would you like to enter a key? 'yes' or 'no': ")
+	key = ""
 	ciphertext = ""
 
-	for i in range(0,len(plaintext)):
-		x = plaintext[i]
-		y = ord(str(x))
-		y -= int(key)
-		ciphertext += chr(y)
-	
-	print(ciphertext)
-	main()
+	if ifkey == 'yes':
+		key = raw_input("Please enter your key: ")
+
+		for i in range(0,len(plaintext)):
+			x = plaintext[i]
+			y = ord(str(x))
+			y -= int(key)
+			ciphertext += chr(y)
+
+		print(ciphertext)
+
+	elif ifkey == 'no':
+		print("Testing all possible solutions...")
+
+		for i in range(1, 32):
+
+			key = i
+
+			for j in range(0,len(plaintext)):
+				x = plaintext[j]
+				y = ord(str(x))
+				y -= int(key)
+				ciphertext += chr(y)
+
+			print(key)	
+			print(ciphertext + "\n")
+
+			ciphertext = ""
 
 init()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
